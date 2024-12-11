541774dd86c0e4130bc2162ce3e5ac69498b2893
Szymon P <szymon_jozef@proton.me>
2024-12-11 13:02:41 +0000

    nwm

---
+++ src/my_project_frontend/src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import { my_project_backend } from 'declarations/my_project_backend/index';
+let greeting = ref('');
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const target = e.target;
+  const name = target.querySelector('#name').value;
+  await my_project_backend.greet(name).then((response) => {
+    greeting.value = response;
+  });
+}
+</script>
+
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <form action="#" @submit="handleSubmit" class="flex flex-col gap-4 justify-center items-center">
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="text" />
+      <button type="submit" class="bg-gradient-to-r from-violet-800 to-violet-850 rounded-lg">Click Me!</button>
+    </form>
+    <section id="greeting">{{ greeting }}</section>
+  </main>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
